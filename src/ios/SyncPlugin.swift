4ca057095cf8f0b4e0ac7f0e5aff719f3fbae392
dineshkokare56 <81946841+dineshkokare56@users.noreply.github.com>
2021-06-10 15:44:08 +0530

    Added code commit.
    Added method of sync plugin.

--- src/ios/SyncPlugin.swift
+++ src/ios/SyncPlugin.swift
@@ -2,7 +2,29 @@ import Foundation
 // Method for SyncPlugin.
 @objc(SyncPlugin) class SyncPlugin : CordovaPlugin {   
 
+  // Method for Sync.
+ @objc(sync:)
+    func sync(_ command: CDVInvokedUrlCommand) {
+        
+    }
 
+    // Method for enqueue.
+     @objc(enqueue:)
+    func enqueue(_ command: CDVInvokedUrlCommand) {
+        
+    }
+
+// Method for onSyncSucces.
+     @objc(onSyncSucces:)
+    func onSyncSucces(_ command: CDVInvokedUrlCommand) {
+        
+    }
+
+    // Method for onAuthorizationError.
+     @objc(onAuthorizationError:)
+    func onAuthorizationError(_ command: CDVInvokedUrlCommand) {
+        
+    }
 
 
 }
